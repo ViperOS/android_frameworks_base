44350e750315732c2c021de55b61b7a0707dfa6e
Cristiano Matos <cristianobmatos@gmail.com>
2016-09-30 20:44:51 +0200

    FWB: Ability to hide superuser status bar icon [1/2]
    - Add a simple option to hide the su icon placed in
    status bar when a session is active
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3910,6 +3910,12 @@ public final class Settings {
          */
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
+	 /**
+          * whether to display SU icon in statusbar
+          * @hide
+          */
+        public static final String SHOW_SU_INDICATOR = "show_su_indicator";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
