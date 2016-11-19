78d3d12e80366aead7548c0bfac2b118aba69eb3
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-19 15:42:25 +0100

    Statusbar: correctly align status bar icons
    Change-Id: I871b3ad79142dfcfd8822df6b3808d5ae563b1cc

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -59,7 +59,7 @@ import org.cyanogenmod.graphics.drawable.StopMotionVectorDrawable;
 public class BatteryMeterDrawable extends Drawable implements
         BatteryController.BatteryStateChangeCallback {
 
-    private static final float ASPECT_RATIO = 9.5f / 14.5f;
+    private static final float ASPECT_RATIO = 9.5f / 14.3f;
     public static final String TAG = BatteryMeterDrawable.class.getSimpleName();
     public static final String SHOW_PERCENT_SETTING = "status_bar_show_battery_percent";
 
