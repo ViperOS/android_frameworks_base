1a99becabeee620f336f9b17a1ccc8c927046c4e
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2017-03-26 00:00:20 -0300

    Fw_base - Battery light: 100% charged level (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3725,6 +3725,12 @@ public final class Settings {
         public static final String STATUS_BAR_DATE_FORMAT = "status_bar_date_format";
 
         /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
