aa91effb96a9cc509f4c044fdd0f80dbde9bb754
maxwen <max.weninger@gmail.com>
2017-04-01 18:41:00 -0300

    OmniJaws qs tile integration [1/3]
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3768,6 +3768,11 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
 
         /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
