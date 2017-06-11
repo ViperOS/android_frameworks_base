24207e326714f9920bfa7cbbfc90c7ff0af252da
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-06-11 16:11:14 +0000

    Revert "Base: API to restart SystemUI without root"
    This reverts commit 6bd6e625e8ecfad555bedd4ab891dfd1bd109173.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -45,7 +45,6 @@ import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
-import android.os.Process;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -1332,12 +1331,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         int msg = MSG_SHOW_PREV_AFFILIATED_TASK;
         mHandler.removeMessages(msg);
         mHandler.sendEmptyMessage(msg);
-    }
-
-    @Override
-    public void restartUI() {
-        Log.d(TAG, "StatusBar API restartUI! Commiting suicide! Goodbye cruel world!");
-        Process.killProcess(Process.myPid());
     }
 
     protected H createHandler() {
