c2bc2f089b585ae06a5c217106eb54444dfd5695
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-06-11 16:03:16 +0000

    Revert "Bluetoothtile: Rectify and fix compilation"
    This reverts commit d96c8ecc980d496dfa3ad3f1a79b2bc0861ba94d.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -285,14 +285,14 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
                         item.icon = R.drawable.ic_qs_bluetooth_connected;
                         item.line2 = mContext.getString(R.string.quick_settings_connected);
                         item.canDisconnect = true;
-                        mBluetoothItems.add(connectedDevices, item);
+                        items.add(connectedDevices, item);
                         connectedDevices++;
                     } else if (state == BluetoothProfile.STATE_CONNECTING) {
                         item.icon = R.drawable.ic_qs_bluetooth_connecting;
                         item.line2 = mContext.getString(R.string.quick_settings_connecting);
-                        mBluetoothItems.add(connectedDevices, item);
+                        items.add(connectedDevices, item);
                     } else {
-                        mBluetoothItems.add(item);
+                        items.add(item);
                     };
                 }
             }
