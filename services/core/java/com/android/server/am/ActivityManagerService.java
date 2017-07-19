c5c700d5a274f7cb1b2be082e2ad9266b6d4545b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-07-19 20:31:16 -0300

    Revert "Handle application crash in new thread."
    Causing crash:
    
    07-19 20:11:46.528   936  8631 W ActivityManager: Failed setting process group of 8570 to 1
    07-19 20:11:46.529   936  8631 W System.err: java.lang.IllegalArgumentException: Given thread 8604 does not exist
    07-19 20:11:46.529   936  8631 W System.err: 	at android.os.Process.setThreadPriority(Native Method)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityManagerService.applyOomAdjLocked(ActivityManagerService.java:20915)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityManagerService.updateOomAdjLocked(ActivityManagerService.java:21454)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityStack.resumeTopActivityInnerLocked(ActivityStack.java:2472)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityStack.resumeTopActivityUncheckedLocked(ActivityStack.java:2137)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityStackSupervisor.resumeFocusedStackTopActivityLocked(ActivityStackSupervisor.java:1843)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityStackSupervisor.resumeFocusedStackTopActivityLocked(ActivityStackSupervisor.java:1833)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityStack.finishCurrentActivityLocked(ActivityStack.java:3681)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityStack.handleAppCrashLocked(ActivityStack.java:5094)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityStackSupervisor.handleAppCrashLocked(ActivityStackSupervisor.java:2900)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.AppErrors.handleAppCrashLocked(AppErrors.java:599)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.AppErrors.makeAppCrashingLocked(AppErrors.java:486)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.AppErrors.crashApplicationInner(AppErrors.java:358)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.AppErrors.crashApplication(AppErrors.java:309)
    07-19 20:11:46.529   936  8631 W System.err: 	at com.android.server.am.ActivityManagerService$19.run(ActivityManagerService.java:13764)
    07-19 20:11:46.529   936  8631 W System.err: 	at java.lang.Thread.run(Thread.java:761)
    
    This reverts commit 1acf3d360aa593b5c1d00618b5b54026c05a41ef.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -13746,8 +13746,8 @@ public final class ActivityManagerService extends ActivityManagerNative
     /* Native crash reporting uses this inner version because it needs to be somewhat
      * decoupled from the AM-managed cleanup lifecycle
      */
-    void handleApplicationCrashInner(String eventType, final ProcessRecord r, String processName,
-            final ApplicationErrorReport.CrashInfo crashInfo) {
+    void handleApplicationCrashInner(String eventType, ProcessRecord r, String processName,
+            ApplicationErrorReport.CrashInfo crashInfo) {
         EventLog.writeEvent(EventLogTags.AM_CRASH, Binder.getCallingPid(),
                 UserHandle.getUserId(Binder.getCallingUid()), processName,
                 r == null ? -1 : r.info.flags,
@@ -13758,12 +13758,7 @@ public final class ActivityManagerService extends ActivityManagerNative
 
         addErrorToDropBox(eventType, r, processName, null, null, null, null, null, crashInfo);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mAppErrors.crashApplication(r, crashInfo);
-            }
-        }).start();
+        mAppErrors.crashApplication(r, crashInfo);
     }
 
     public void handleApplicationStrictModeViolation(
