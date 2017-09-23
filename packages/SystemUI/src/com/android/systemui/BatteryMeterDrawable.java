4a47c1ea2f3bf62ecc14f5f8b7a33fbcd8d2a1ca
Michael W <baddaemon87@gmail.com>
2017-09-23 15:39:11 +0200

    SystemUI: Improve battery icons
    * When the battery saver is on, always show the percentage (if option is
      set)
    * Change color of exclamation mark when the battery saver is on so it's
      visible
    
    Change-Id: Ia7e6bc05225b131aaa62370314b035f2b874b5ec

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -213,7 +213,6 @@ public class BatteryMeterDrawable extends Drawable implements
         mTextAndBoltPaint.setColor(mBoltOverlay || mCurrentFillColor == 0 ? getBoltColor() : mCurrentFillColor);
 
         mWarningTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mWarningTextPaint.setColor(mColors[1]);
         font = Typeface.create("sans-serif", Typeface.BOLD);
         mWarningTextPaint.setTypeface(font);
         mWarningTextPaint.setTextAlign(getPaintAlignmentFromGravity(mTextGravity));
@@ -746,7 +745,7 @@ public class BatteryMeterDrawable extends Drawable implements
 
     private void drawPercentageText(Canvas canvas) {
         final int level = mLevel;
-        if (level > mCriticalLevel && mShowPercent && level != 100) {
+        if ((level > mCriticalLevel || mPowerSaveEnabled) && mShowPercent && level != 100) {
             // Draw the percentage text
             String pctText = String.valueOf(SINGLE_DIGIT_PERCENT ? (level / 10) : level);
             mTextAndBoltPaint.setColor(getColorForLevel(level));
@@ -756,6 +755,9 @@ public class BatteryMeterDrawable extends Drawable implements
             }
         } else if (level <= mCriticalLevel) {
             // Draw the warning text
+            mWarningTextPaint.setColor(mPowerSaveEnabled
+                    ? mColors[mColors.length - 1]
+                    : mColors[1]);
             canvas.drawText(mWarningString, mTextX, mTextY, mWarningTextPaint);
         }
     }
