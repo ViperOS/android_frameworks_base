ab05771edd82269460e158bdde237fc521fa67b4
maxwen <max.weninger@gmail.com>
2018-01-08 02:52:58 +0100

    base: persist screenshot type in settings
    save state set in qs tile into settings
    
    Change-Id: Ieca6148862386cef01767d43938bb803aab282cb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4338,6 +4338,13 @@ public final class Settings {
         public static final String FP_SUCCESS_VIBRATE = "fp_success_vibrate";
 
         /**
+         * 0 - fullscreen
+         * 1 - partial
+         * @hide
+         */
+        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
