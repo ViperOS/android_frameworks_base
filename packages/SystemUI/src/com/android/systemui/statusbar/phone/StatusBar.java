e547455eee1202613ae52b6034ff77e158f87704
Alex Cruz <alex@dirtyunicorns.com>
2018-11-15 21:59:40 +0100

    Power save: when disabling it, go back to the previous set theme mode
    Change-Id: I94e0a19bce5721724b72005b3cd798d63415563b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -52,6 +52,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.StatusBarManager;
 import android.app.TaskStackBuilder;
+import android.app.UiModeManager;
 import android.app.WallpaperColors;
 import android.app.WallpaperInfo;
 import android.app.WallpaperManager;
@@ -461,6 +462,8 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
     private boolean mBrightnessControl;
     private boolean mBrightnessChanged;
     private boolean mJustPeeked;
+
+    private int mPreviousDarkMode;
 
     /**
      * Helper that is responsible for showing the right toast when a disallowed activity operation
@@ -1000,9 +1003,23 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 if (mDozeServiceHost != null) {
                     mDozeServiceHost.firePowerSaveChanged(isPowerSave);
                 }
+
                 if (NIGHT_MODE_IN_BATTERY_SAVER) {
-                    mContext.getSystemService(UiModeManager.class).setNightMode(
-                        isPowerSave ? UiModeManager.MODE_NIGHT_YES : UiModeManager.MODE_NIGHT_NO);
+                    final UiModeManager umm = mContext.getSystemService(UiModeManager.class);
+                    if (isPowerSave) {
+                        mPreviousDarkMode = umm.getNightMode();
+                    }
+                    switch (mPreviousDarkMode) {
+                        case UiModeManager.MODE_NIGHT_AUTO:
+                        case UiModeManager.MODE_NIGHT_NO:
+                           umm.setNightMode(
+                                    isPowerSave ? UiModeManager.MODE_NIGHT_YES :
+                                            mPreviousDarkMode);
+                            break;
+                        case UiModeManager.MODE_NIGHT_YES:
+                            // do nothing, the user forced dark mode
+                            break;
+                    }
                 }
             }
 
